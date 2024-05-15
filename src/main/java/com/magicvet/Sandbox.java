957f7c43b1aee9a9e1bdab7961df2f6ad074b49e
vbraychenko <rybachok@ukr.net>
2024-05-15 14:45:06 +0300

    REQ-09: Add registration date for Client class

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,9 +1,10 @@
 package main.java.com.magicvet;
 
-import main.java.com.magicvet.model.Cat;
+import main.java.com.magicvet.model.Client;
 
 public class Sandbox {
     public static void main(String[] args) {
-        System.out.println(new Cat());
+
+        System.out.println(new Client());
     }
 }
