030a64f951d2684e91922d4b25c5ac7baea75b55
vbraychenko <rybachok@ukr.net>
2024-05-24 15:41:44 +0300

    REQ-09: Ability to add a few pets per client.

--- src/main/java/com/magicvet/model/Client.java
+++ src/main/java/com/magicvet/model/Client.java
@@ -2,6 +2,8 @@ package main.java.com.magicvet.model;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Client {
@@ -9,7 +11,7 @@ public class Client {
     private String firstName;
     private String lastName;
     private String email;
-    private Pet pet;
+    private List<Pet> pets = new ArrayList<>();
     private final LocalDateTime clientRegistrationDate = LocalDateTime.now();
 
     @Override
@@ -19,7 +21,7 @@ public class Client {
                 + ", lastName = " + lastName
                 + ", email = " + email
                 + ", clientRegistrationDate = " + clientRegistrationDate.format(FORMATTER)
-                + ",\n\tpet = " + pet
+                + ",\n\tpets = " + pets
                 + "\n}";
     }
 
@@ -31,13 +33,13 @@ public class Client {
         return Objects.equals(firstName, client.firstName)
                 && Objects.equals(lastName, client.lastName)
                 && Objects.equals(email, client.email)
-                && Objects.equals(pet, client.pet);
+                && Objects.equals(pets, client.pets);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(firstName, lastName, email, pet);
+        return Objects.hash(firstName, lastName, email, pets);
     }
 
     public void setFirstName(String firstName) {
@@ -70,12 +72,18 @@ public class Client {
         return email;
     }
 
-    public Pet getPet() {
-        return pet;
+    public List<Pet> getPet() {
+
+        return pets;
     }
 
-    public void setPet(Pet pet) {
-        this.pet = pet;
+    public void setPet(List<Pet> pets) {
+
+        this.pets = pets;
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
     }
 }
 
